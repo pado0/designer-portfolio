3a47db941c5e22d7b70656e8e3c5e08c423efcf5
pado0 <bgshhd95@gmail.com>
2025-04-30 23:38:54 -0700

    feat: remove animation on home section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,38 +6,9 @@ import ProjectSection from "../components/ProjectSection.tsx";
 const Home = () => {
     const sectionsRef = useRef<(HTMLDivElement | null)[]>([]);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        entry.target.classList.add("visible");
-                    } else {
-                        entry.target.classList.remove("visible");
-                    }
-                });
-            },
-            { 
-                threshold: 0.1,
-                rootMargin: "-50px 0px" // Add some margin to make the animation smoother
-            }
-        );
-
-        sectionsRef.current.forEach((section) => {
-            if (section) observer.observe(section);
-        });
-
-        return () => {
-            observer.disconnect();
-        };
-    }, []);
-
     return (
         <div className="Home">
-            <div ref={(el) => (sectionsRef.current[0] = el)} className="section">
                 <HeroSection />
-            </div>
-            <div ref={(el) => (sectionsRef.current[1] = el)} className="section">
                 <ProjectSection
                     title="Featured Work"
                     description="From concept to completion, these projects highlight my expertise in Product Design."
@@ -64,8 +35,6 @@ const Home = () => {
                         },
                     ]}
                 />
-            </div>
-            <div ref={(el) => (sectionsRef.current[2] = el)} className="section">
                 <ProjectSection
                     title="Beyond the Interface"
                     description="I also explore user research and seasonal event to create impactful user experiences."
@@ -83,7 +52,6 @@ const Home = () => {
                     ]}
                 />
             </div>
-        </div>
     );
 };
 
